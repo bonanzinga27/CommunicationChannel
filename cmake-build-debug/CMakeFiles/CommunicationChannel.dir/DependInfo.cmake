30afb04815b521dffe401739d924914f983d6dd5
bonanzinga27 <bonanzinga27@gmail.com>
2017-07-06 18:15:47 +0200

    idea project file

---
+++ cmake-build-debug/CMakeFiles/CommunicationChannel.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/andrea/Documents/GIT/CommunicationChannel/CRC_16-CCITT/crc_16ccitt.c" "/home/andrea/Documents/GIT/CommunicationChannel/cmake-build-debug/CMakeFiles/CommunicationChannel.dir/CRC_16-CCITT/crc_16ccitt.c.o"
+  "/home/andrea/Documents/GIT/CommunicationChannel/CommunicationFunctions/serial_communication_functions.c" "/home/andrea/Documents/GIT/CommunicationChannel/cmake-build-debug/CMakeFiles/CommunicationChannel.dir/CommunicationFunctions/serial_communication_functions.c.o"
+  "/home/andrea/Documents/GIT/CommunicationChannel/main.c" "/home/andrea/Documents/GIT/CommunicationChannel/cmake-build-debug/CMakeFiles/CommunicationChannel.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CommunicationChannel.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CommunicationChannel.dir/CRC_16-CCITT/crc_16ccitt.c.o"
+  "CMakeFiles/CommunicationChannel.dir/CommunicationFunctions/serial_communication_functions.c.o"
+  "CMakeFiles/CommunicationChannel.dir/main.c.o"
+  "CommunicationChannel.pdb"
+  "CommunicationChannel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CommunicationChannel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
